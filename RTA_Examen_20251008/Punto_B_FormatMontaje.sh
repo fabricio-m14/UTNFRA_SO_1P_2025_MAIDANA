fa51d1fda8c487489779621f7afa560ac07d36ff
fabricio-m14 <fabriciomaidanaa14@gmail.com>
2025-10-08 17:33:47 -0300

    Update Punto_B_FormatMontaje.sh

--- RTA_Examen_20251008/Punto_B_FormatMontaje.sh
+++ RTA_Examen_20251008/Punto_B_FormatMontaje.sh
@@ -7,7 +7,7 @@
 
 DISCO= "/dev/sdc"
 
-echo= "--- INICIALIZANDO FORMATEO DE PARTICIONES ---"
+echo "--- INICIALIZANDO FORMATEO DE PARTICIONES ---"
 
 sudo mkfs.ext4 -F /dev/sdc1
 sudo mkfs.ext4 -F /dev/sdc2
@@ -15,7 +15,7 @@ sudo mkfs.ext4 -F /dev/sdc5
 sudo mkfs.ext4 -F /dev/sdc6
 sudo mkfs.ext4 -F /dev/sdc7
 
-echo= "--- PUNTO DE MONTAJE ---"
+echo "--- PUNTO DE MONTAJE ---"
 
 sudo mkdir -p /mnt/Planeta/Gravedad
 sudo mkdir -p /mnt/Planeta/Orbita
@@ -23,7 +23,7 @@ sudo mkdir -p /mnt/Cometa/Cola
 sudo mkdir -p /mnt/Cometa/Gravedad
 sudo mkdir -p /mnt/Cometa/Orbita
 
-echo= "--- MONTAJE ---"
+echo "--- MONTAJE ---"
 
 sudo mount /dev/sdc1 /mnt/Planeta/Graavedad
 sudo mount /dev/sdc2 /mnt/Planeta/Orbita
@@ -31,7 +31,7 @@ sudo mount /dev/sdc5 /mnt/Cometa/Cola
 sudo mount /dev/sdc6 /mnt/Cometa/Gravedad
 sudo mount /dev/sdc7 /mnt/Cometa/Orbita
 
-echo= "--- MONTAJE PERSISTENTE ---"
+echo "--- MONTAJE PERSISTENTE ---"
 
 sudo cp /etc/fstab /etc/fstab.bak
 
@@ -41,7 +41,7 @@ UUID5=$(sudo blkid -s UUID -o value /dev/sdc5)
 UUID6=$(sudo blkid -s UUID -o value /dev/sdc6)
 UUID7=$(sudo blkid -s UUID -o value /dev/sdc7)
 
-echo= "--- AGREGANDO ID´S A /etc/fstab ---"
+echo "--- AGREGANDO ID´S A /etc/fstab ---"
 
 echo "UUID=$UUID1  /mnt/Planeta/Gravedad  ext4  defaults  0  2" | sudo tee -a /etc/fstab
 echo "UUID=$UUID2  /mnt/Planeta/Orbita  ext4  defaults  0  2" | sudo tee -a /etc/fstab
